6bf0c1a57b3d7d88538d99ce22db09a54134a765
Szymon Piątek <szymon.piatek125@gmail.com>
2024-01-04 21:25:18 +0100

    Added customtkinter GUI

---
+++ functions.py
@@ -0,0 +1,5 @@
+from random import randint
+
+
+def throw_dice():
+    return randint(1, 6) + randint(1, 6)
---
+++ settings.py
@@ -0,0 +1,23 @@
+# Names
+NAMES = ["Peter", "Adam", "John", "Smith", "Juan Pablo", "Leo", "Michael", "Daniel", "Bob", "Ted"]
+
+# Colors
+COLOR_1 = "#262626"
+COLOR_2 = "#525252"
+COLOR_3 = "#4bc71a"     # Green
+COLOR_4 = "#d65b0f"     # Orange
+COLOR_5 = "#1f7ead"     # Light Blue
+COLOR_6 = "#8e3dd4"     # Light Purple
+COLOR_7 = "#c29d23"     # Yellow
+
+LABEL_COLOR_1 = COLOR_2
+
+START_COLOR_1 = COLOR_3
+PRISON_COLOR_1 = COLOR_4
+PARKING_COLOR_1 = COLOR_7
+TAX_COLOR_1 = COLOR_4
+TAKE_CARD_COLOR_1 = COLOR_6
+COUNTRY_COLOR_1 = COLOR_5
+
+# Fonts
+FONT_1 = "Helvetica"
